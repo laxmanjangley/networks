962b6847721bb364ac2610b055cd861b221d325a
Manav <manavjinger@gmail.com>
2015-10-03 14:14:55 +0530

    v1.0

--- parser.py
+++ parser.py
@@ -11,30 +11,47 @@ def buildjson (a):
 types = []
 size = {}
 ip = {}
-y = 0
+
+dnsTime = {}
+
+#return tree , page load time
 def buildTree (a):
+	pageloadTime = 0
 	jsonObject = buildjson(a)
 	tree = {}
 	entries = jsonObject['log']['entries']
 	for i in entries:
 		curr = i['request']['url']
 		x = ''
+		#3 c i
+		pageloadTime += i['time']
+		# 3 c ii
+		if(not(curr in dnsTime)) :
+			dnsTime[curr] = i['timings']['dns']
+
 		if(not(curr in tree)):
 			for j in i['request']['headers']:
 				if(j['name'] == 'Referer'):
 					tree[curr] = j['value']
+					if(not(i['serverIPAddress'] in ip)):
+						ip[i['serverIPAddress']] =j['value'] 
 		if(not(curr in size)):
 			size[curr] = i['response']['bodySize']
 		elif(i['response']['bodySize'] > 0):
 			size[curr] +=i['response']['bodySize']
+
+		# types
 		for j in range(len(curr) - 5 , len(curr)-1 ):
 			if(curr[j] == '.'):
 				x = curr[j:]
 				break
 		if(not (x in types or x == '')):
 			types.append(x)
-	return tree
+	# print pageloadTime
+	#print dnsTime
+	return tree , pageloadTime
 def reset():
 	types =[]
 	ip = {}
 	size = {}
+	dnsTime = {}
