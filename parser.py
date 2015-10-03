6e04fef2c07aa1bb9cc13886db01ed3ef127b39d
Manav <manavjinger@gmail.com>
2015-10-03 12:01:04 +0530

    correction

--- parser.py
+++ parser.py
@@ -9,37 +9,32 @@ def buildjson (a):
     	File.close() ;
 	return jsonObject
 types = []
-size = {} 
+size = {}
 ip = {}
+y = 0
 def buildTree (a):
-    jsonObject = buildjson(a)
-    tree = {}
-    entries = jsonObject['log']['entries']
-    for i in entries:
-        curr = i['request']['url']
-	x= ''
-	if(not (curr in tree)):
-		for j in i['request']['headers']:
-		    if(j['name'] == 'Referer' ):
-		    	tree[curr] =  j['value'] 
-			ip[i['serverIPAddress']] = curr			
-	if((not (curr in size)) and  (i['response']['bodySize'] > 0 )):
-		size[curr] = i['response']['bodySize']
-	elif (i['response']['bodySize'] > 0):
-		size[curr] += i['response']['bodySize']
-	for j in range(len(curr) -1 , len(curr) - 5 , -1):
-		if (curr[j] == '.'):
-			x = curr[j:]
-			break
-	if(not(x in types) and  (not(x==''))):
-		types.append(x)	
-    return tree
+	jsonObject = buildjson(a)
+	tree = {}
+	entries = jsonObject['log']['entries']
+	for i in entries:
+		curr = i['request']['url']
+		x = ''
+		if(not(curr in tree)):
+			for j in i['request']['headers']:
+				if(j['name'] == 'Referer'):
+					tree[curr] = j['value']
+		if(not(curr in size)):
+			size[curr] = i['response']['bodySize']
+		elif(i['response']['bodySize'] > 0):
+			size[curr] +=i['response']['bodySize']
+		for j in range(len(curr) - 5 , len(curr)-1 ):
+			if(curr[j] == '.'):
+				x = curr[j:]
+				break
+		if(not (x in types or x == '')):
+			types.append(x)
+	return tree
 def reset():
 	types =[]
 	ip = {}
 	size = {}
-#tree =  buildTree (sys.argv[1])
-#noOfObjects = len (tree)
-#print types
-#print size
-
